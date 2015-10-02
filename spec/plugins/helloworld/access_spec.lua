ef87bbe2a7504ee76182deeadeab8d812983decf
Jason Riddle <jr1285@vt.edu>
2015-10-01 21:43:52 -0400

    Add tests for helloworld plugin

---
+++ spec/plugins/helloworld/access_spec.lua
@@ -0,0 +1,44 @@
+local spec_helper = require "spec.spec_helpers"
+local http_client = require "kong.tools.http_client"
+
+local STUB_GET_URL = spec_helper.STUB_GET_URL
+local STUB_POST_URL = spec_helper.STUB_POST_URL
+
+describe("HelloWorld Plugin", function()
+
+  setup(function()
+    spec_helper.prepare_db()
+    spec_helper.insert_fixtures {
+      api = {
+        {name = "tests helloworld 1", request_host = "helloworld1.com", upstream_url = "http://mockbin.com"},
+        {name = "tests helloworld 2", request_host = "helloworld2.com", upstream_url = "http://mockbin.com"}
+      },
+      consumer = {
+      },
+      plugin_configuration = {
+        {name = "helloworld", value = {say_hello = true }, __api = 1},
+        {name = "helloworld", value = {say_hello = false }, __api = 2},
+      }
+    }
+
+    spec_helper.start_kong()
+  end)
+
+  teardown(function()
+    spec_helper.stop_kong()
+  end)
+
+  describe("Response", function()
+     it("should return an Hello-World header with Hello World!!! value when say_hello is true", function()
+      local _, status, headers = http_client.get(STUB_GET_URL, {}, {host = "helloworld1.com"})
+      assert.are.equal(200, status)
+      assert.are.same("Hello World!!!", headers["hello-world"])
+    end)
+
+    it("should return an Hello-World header with Bye World!!! value when say_hello is false", function()
+      local _, status, headers = http_client.get(STUB_GET_URL, {}, {host = "helloworld2.com"})
+      assert.are.equal(200, status)
+      assert.are.same("Bye World!!!", headers["hello-world"])
+    end)
+  end)
+end)
